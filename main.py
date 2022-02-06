a3d771fb09f323ea816932f49c2dbace371093ab
Kazumasa Kusaba <kazumasa@mail.kusaba.xyz>
2022-02-06 13:06:31 +0900

    replace "maze" with "puzzle"

--- main.py
+++ main.py
@@ -3,24 +3,24 @@ import copy
 
 directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
-def isFilled(maze: List[List[int]]) -> bool:
-  for row_index in range(len(maze)):
-    for col_index in range(len(maze[0])):
-      if maze[row_index][col_index] == 0:
+def isFilled(puzzle: List[List[int]]) -> bool:
+  for row_index in range(len(puzzle)):
+    for col_index in range(len(puzzle[0])):
+      if puzzle[row_index][col_index] == 0:
         return False
   return True
 
-def brute_force_sub(row_index: int, col_index: int, maze: List[List[int]], ans: List[tuple]) -> bool:
-  if row_index < 0 or len(maze) <= row_index:
+def brute_force_sub(row_index: int, col_index: int, puzzle: List[List[int]], ans: List[tuple]) -> bool:
+  if row_index < 0 or len(puzzle) <= row_index:
     return False
-  if col_index < 0 or len(maze[0]) <= col_index:
+  if col_index < 0 or len(puzzle[0]) <= col_index:
     return False
-  if maze[row_index][col_index] == 1:
+  if puzzle[row_index][col_index] == 1:
     return False
 
-  maze[row_index][col_index] = 1
+  puzzle[row_index][col_index] = 1
   
-  if isFilled(maze):
+  if isFilled(puzzle):
     return True
   
   for direction in directions:
@@ -28,21 +28,21 @@ def brute_force_sub(row_index: int, col_index: int, maze: List[List[int]], ans: 
     row_index += direction[0]
     col_index += direction[1]
     ans.append((col_index, row_index))
-    if brute_force_sub(row_index, col_index, maze, ans):
+    if brute_force_sub(row_index, col_index, puzzle, ans):
       return True
     ans.pop()
     row_index, col_index = before_row_index, before_col_index
   
   return False
 
-def brute_force(maze: List[List[int]], ans: List[tuple]) -> bool:
-  checked = copy.deepcopy(maze)
-  for row_index in range(len(maze)):
-    for col_index in range(len(maze[0])):
+def brute_force(puzzle: List[List[int]], ans: List[tuple]) -> bool:
+  checked = copy.deepcopy(puzzle)
+  for row_index in range(len(puzzle)):
+    for col_index in range(len(puzzle[0])):
       if checked[row_index][col_index] == 0:
         checked[row_index][col_index] == 1
         ans.append((col_index, row_index))
-        if brute_force_sub(row_index, col_index, maze, ans):
+        if brute_force_sub(row_index, col_index, puzzle, ans):
           return True
         ans.pop()
   return False
@@ -50,24 +50,24 @@ def brute_force(maze: List[List[int]], ans: List[tuple]) -> bool:
 if __name__ == '__main__':
   ans = []
 
-  # maze
+  # puzzle
   # 0: route
   # 1: obstacle or the place user took
-  maze = [[1, 1, 0, 0, 0],\
+  puzzle = [[1, 1, 0, 0, 0],\
           [1, 1, 1, 1, 0],\
           [0, 0, 0, 1, 0],\
           [0, 0, 0, 0, 0],\
           [0, 0, 0, 0, 0]]
   
-  for row_index in range(len(maze)):
-    for col_index in range(len(maze[0])):
-      print(str(maze[row_index][col_index]) + " ", end="")
+  for row_index in range(len(puzzle)):
+    for col_index in range(len(puzzle[0])):
+      print(str(puzzle[row_index][col_index]) + " ", end="")
     print()
 
-  ret = brute_force(maze, ans)
+  ret = brute_force(puzzle, ans)
   if ret:
-    print("This maze can be solved. The route is below.")
+    print("This puzzle can be solved. The route is below.")
     print(ans)
   else:
-    print("This maze can not be solved.")
+    print("This puzzle can not be solved.")
 
